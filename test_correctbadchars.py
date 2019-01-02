10e29d4f07cf75759ba985f7ea8fac8cf0e12534
apulai <andras.pulai@gmail.com>
2019-01-02 13:43:28 +0100

    Trying to add a test part

--- test_correctbadchars.py
+++ test_correctbadchars.py
@@ -1,5 +1,6 @@
 import mp3tagger
 import glob
+import json
 
 PATH = "Z:\\mp3\\_Magyar\\Valami Amerika"
 
@@ -17,6 +18,8 @@ print("Found {} directories to scan".format(number_of_directories_found))
 for current_directory in directories:
     song_list = mp3tagger.collect_mp3info(PATH)
     ret_song_list = mp3tagger.remove_bad_chars(song_list)
+    json.dumps(ret_song_list, indent=4, ensure_ascii=False))
+    exit(2)
     mp3tagger.rewrite_songs_with_bad_chars(ret_song_list)
     number_of_directories_found = number_of_directories_found - 1;
     print("Number of directories to go {}".format(number_of_directories_found))
